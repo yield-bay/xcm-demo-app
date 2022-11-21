59ecbd730789d5af15812cbed07fd3baea2580ab
chrisli30 <chrisli30@users.noreply.github.com>
2022-11-20 21:36:16 -0800

    Remove redundant files

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,8 +1,7 @@
 import useWindowSize from './useWindowSize';
-import usePromotionalMilestones from './usePromotionalMilestones';
 import useSubscribeBalance from './useSubscribeBalance';
 import useSubscribeXtokenBalance from './useSubscribeXtokenBalance';
 
 export {
-  useWindowSize, usePromotionalMilestones, useSubscribeBalance, useSubscribeXtokenBalance,
+  useWindowSize, useSubscribeBalance, useSubscribeXtokenBalance,
 };
